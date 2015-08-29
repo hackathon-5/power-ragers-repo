625573f5a88260e9f7f98f805e227af3850eaca3
Andrew Askins <r.andrew.askins@gmail.com>
2015-08-29 03:07:12 -0400

    Templates for orders

--- web/app/routes/orders.js
+++ web/app/routes/orders.js
@@ -1,4 +1,55 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function() {
+		return [{
+			itemName: 'Mac & Cheese',
+			createdAt: 'placed 4 minutes ago',
+			customerName: 'Truck Harrison',
+			open: true,
+			price: 5.50
+		},
+		{
+			itemName: 'Mac & Cheese',
+			createdAt: 'placed 4 minutes ago',
+			customerName: 'Truck Harrison',
+			open: true,
+			price: 5.50
+		},
+		{
+			itemName: 'Mac & Cheese',
+			createdAt: 'placed 4 minutes ago',
+			customerName: 'Truck Harrison',
+			open: true,
+			price: 5.50
+		},
+		{
+			itemName: 'Mac & Cheese',
+			createdAt: 'placed 4 minutes ago',
+			customerName: 'Truck Harrison',
+			open: false,
+			price: 5.50
+		},
+		{
+			itemName: 'Mac & Cheese',
+			createdAt: 'placed 4 minutes ago',
+			customerName: 'Truck Harrison',
+			open: false,
+			price: 5.50
+		},
+		{
+			itemName: 'Mac & Cheese',
+			createdAt: 'placed 4 minutes ago',
+			customerName: 'Truck Harrison',
+			open: false,
+			price: 5.50
+		},
+		{
+			itemName: 'Mac & Cheese',
+			createdAt: 'placed 4 minutes ago',
+			customerName: 'Truck Harrison',
+			open: false,
+			price: 5.50
+		}];	
+	}
 });
